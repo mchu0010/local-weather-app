bb0ebe393fdaea9c97bac9dff7c7b4b9e96cbff3
Mike Chu <m.chu90@gmail.com>
2016-05-23 17:50:10 -0500

    created new files and first html commit

---
+++ weather-app.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="weather-app.css">
+    <script src="weather-app.js"></script>
+  </head>
+  
+  <body>
+    <div id="title">Local Weather App</div>    
+    <div id="date"></div>
+</html>
+
